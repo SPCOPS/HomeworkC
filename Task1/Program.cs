5abffed1a13496dfa1539e60640182a1c597f6c2
SPECTR <ivanov8465@mail.ru>
2023-03-05 17:14:16 +0300

    Задание 1 выполнено

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (m > n)
+    Console.WriteLine($"max = {m}");
+else
+    Console.WriteLine($"max = {n}");
