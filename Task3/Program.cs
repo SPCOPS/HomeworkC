9bf7680664d1af8e04c7115802ece9f77bc1a608
SPECTR <ivanov8465@mail.ru>
2023-03-05 17:21:51 +0300

    Задание 3 выполнено

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int ost = n % 2;
+if (ost == 0)
+Console.WriteLine("Да, число четное");
+else
+Console.WriteLine("Нет, число нечетное");
